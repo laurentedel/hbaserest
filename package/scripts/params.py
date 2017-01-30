98e1a6db824fa8110edb1c9918be680465c6c9a3
Laurent Edel <laurent.edel@gmail.com>
2017-01-30 17:40:32 +0100

    Update params.py
    change rest & info ports, pid_dir from hbase-env config

--- package/scripts/params.py
+++ package/scripts/params.py
@@ -5,8 +5,9 @@ from resource_management import *
 config = Script.get_config()
 
 script_path = config['configurations']['hbaserest-config']['hbase.rest.path']
-rest_port = config['configurations']['hbaserest-config']['hbase.rest.port']
-#pid_dir = config['configurations']['hbase-env']['hbase_pid_dir']
-pid_dir = config['configurations']['hbaserest-config']['hbase.pid.dir']
+rest_port = config['configurations']['hbase-site']['hbase.rest.port']
+info_port = config['configurations']['hbase-site']['hbase.rest.info.port']
+#pid_dir = config['configurations']['hbaserest-config']['hbase.pid.dir']
+pid_dir = config['configurations']['hbase-env']['hbase_pid_dir']
 #hbase_user = config['configurations']['hbase-env']['hbase_user']
 hbase_user = config['configurations']['hbaserest-config']['hbase.user']
